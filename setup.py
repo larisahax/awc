e20c5e7f5ceda009861968e3185e7fbcad76fcc7
= <=>
2017-03-14 20:21:57 +0100

    make a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='awc',
+      version='0.1',
+      description='Adaptive Weights Clustering',
+      url='https://github.com/larisahax/awc',
+      author='Efimov K., Adamyan L., Spokoiny V.',
+      author_email='ladamyan@hu-berlin.de',
+      license='MIT',
+      packages=['awc'],
+      zip_safe=False)
